1b147436867292a166d8f5712818698cf4f5d2dd
rafael <rafd1788@gmail.com>
2016-11-28 21:07:31 -0500

    Add RockEngine class

--- src/main/java/asteriods/asteriods/MainApp.java
+++ src/main/java/asteriods/asteriods/MainApp.java
@@ -1,16 +1,9 @@
 package asteriods.asteriods;
 
-import asteriods.elements.Asteriod;
-import asteriods.rockengine.CollisionDetector;
-import asteriods.rockengine.Point;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import asteriods.rockengine.RockEngine;
 import java.util.Timer;
-import java.util.TimerTask;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
-import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -22,51 +15,39 @@ public class MainApp extends Application {
         //Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene.fxml"));
         VBox root = new VBox();
         Scene scene = new Scene(root, 600, 600);
-
-        Asteriod a = new Asteriod();
-        a.setManaged(false);
-
-        Asteriod b = new Asteriod();
-        b.setManaged(false);
-        
-        Asteriod c = new Asteriod();
-        c.setManaged(false);
-
-        root.getChildren().add(a);
-        root.getChildren().add(b);
-        root.getChildren().add(c);
+        RockEngine rockEngine = new RockEngine(root, 10);
+        rockEngine.initializeAsteriods();
         scene.getStylesheets().add("/styles/Styles.css");
 
         stage.setTitle("JavaFX and Maven");
         stage.setScene(scene);
         stage.show();
-        
-        a.setRandomPath();
-        b.setRandomPath();
-        c.setRandomPath();
 
         Timer timer = new Timer();
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                Platform.runLater(()-> {
-                    a.updatePosition();
-                    b.updatePosition();
-                    c.updatePosition();
-                    
-                    CollisionDetector collisionDetector = new CollisionDetector(600, 600);
-                    collisionDetector.addElement(a);
-                    collisionDetector.addElement(b);
-                    collisionDetector.addElement(c);
-                    
-                    Set<Integer> index = collisionDetector.getCrashedElements();
-                    List<Integer> indexOfCrashedElements = new ArrayList<>(index);
-                    if (!indexOfCrashedElements.isEmpty()){
-                        timer.cancel();
-                    }
-                });
-            }
-        }, 50, 50);
+        timer.schedule(rockEngine, 50, 50);
+//        timer.schedule(new TimerTask() {
+//            @Override
+//            public void run() {
+//                Platform.runLater(()-> {
+//                    a.updatePosition();
+//                    b.updatePosition();
+//                    c.updatePosition();
+//                    d.updatePosition();
+//                    
+//                    CollisionDetector collisionDetector = new CollisionDetector(600, 600);
+//                    collisionDetector.addElement(a);
+//                    collisionDetector.addElement(b);
+//                    collisionDetector.addElement(c);
+//                    collisionDetector.addElement(d);
+//                    
+//                    Set<Integer> index = collisionDetector.getCrashedElements();
+//                    List<Integer> indexOfCrashedElements = new ArrayList<>(index);
+//                    if (!indexOfCrashedElements.isEmpty()){
+//                        timer.cancel();
+//                    }
+//                });
+//            }
+//        }, 50, 50);
 
     }
 
---
+++ src/main/java/asteriods/rockengine/RockEngine.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package asteriods.rockengine;
+
+import asteriods.elements.Asteriod;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TimerTask;
+import javafx.application.Platform;
+import javafx.scene.layout.VBox;
+
+/**
+ *
+ * @author rafael
+ */
+public class RockEngine extends TimerTask {
+
+    private CollisionDetector collisionDetector;
+    private List<Asteriod> asteriods;
+    private int numberOfAsteriods;
+    private VBox root;
+
+    public RockEngine(VBox root, int numberOfAsteriods) {
+        this.numberOfAsteriods = numberOfAsteriods;
+        this.root = root;
+    }
+
+    public void initializeAsteriods() {
+        asteriods = new ArrayList<>();
+        for (int i = 0; i < this.numberOfAsteriods; i++) {
+            Asteriod asteriod = new Asteriod();
+            asteriods.add(asteriod);
+            asteriod.setManaged(false);
+            root.getChildren().add(asteriod);
+            asteriod.setRandomPath();
+        }
+    }
+
+    public void updateAsteriodsPositions() {
+        for (int i = 0; i < this.numberOfAsteriods; i++) {
+            asteriods.get(i).updatePosition();
+        }
+    }
+
+    public void processCollisionDetector() {
+        this.collisionDetector = new CollisionDetector(600, 600);
+        for (int i = 0; i < this.numberOfAsteriods; i++) {
+            this.collisionDetector.addElement(asteriods.get(i));
+        }
+    }
+
+    @Override
+    public void run() {
+        Platform.runLater(() -> {
+            updateAsteriodsPositions();
+            processCollisionDetector();
+            Set<Integer> index = collisionDetector.getCrashedElements();
+            List<Integer> indexOfCrashedElements = new ArrayList<>(index);
+            if (!indexOfCrashedElements.isEmpty()) {
+                
+            }
+        });
+    }
+}
