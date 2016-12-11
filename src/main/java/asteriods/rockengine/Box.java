52fff4c08a6e00d40d13e5cb9dc252547d56503b
rafael <rafd1788@gmail.com>
2016-12-11 13:38:46 -0500

    Box class

---
+++ src/main/java/asteriods/rockengine/Box.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package asteriods.rockengine;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author rafael
+ */
+public class Box {
+    private List<Point> corners;
+    private int[] coordinates;
+    
+    public Box (int [] coordinates){
+        corners = new ArrayList<>();
+        corners.add(new Point (coordinates[0], coordinates[2]));
+        corners.add(new Point (coordinates[1], coordinates[2]));
+        corners.add(new Point (coordinates[1], coordinates[3]));
+        corners.add(new Point (coordinates[0], coordinates[3]));
+        this.coordinates = coordinates;
+    }
+    
+    public boolean isBoxOverlapped(Box box){
+        
+        for (int i=0; i<box.getCorners().size(); i++){
+            Point boxCorner = box.getCorners().get(i);
+            if (isPointInsideBox(boxCorner)){
+                return true;
+            }
+        }
+        
+        return false;
+    }
+
+    public boolean isPointInsideBox(Point point){
+        
+        if (this.coordinates[0] > point.getX()){
+            return false;
+        }
+                    
+        if (this.coordinates[1] < point.getX()){
+            return false;
+        }
+        
+        if (this.coordinates[2] > point.getY()){
+            return false;
+        }
+                    
+        if (this.coordinates[3] < point.getY()){
+            return false;
+        }
+        
+        return true;
+    }
+    
+    public List<Point> getCorners() {
+        return corners;
+    }
+}
---
+++ src/test/java/asteriods/rockengine/BoxTest.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package asteriods.rockengine;
+
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author rafael
+ */
+public class BoxTest {
+    
+    public BoxTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of isBoxOverlapped method, of class Box.
+     */
+    @Test
+    public void testIsBoxOverlapped() {
+        System.out.println("isBoxOverlapped");
+        Box a = new Box(new int [] {9, 14, 3, 7});
+        Box b = new Box(new int [] {13, 19, 1, 4});
+        
+        assertTrue(a.isBoxOverlapped(b));
+    }
+    
+    @Test
+    public void testIsBoxOverlapped_false() {
+        System.out.println("isBoxOverlapped");
+        Box a = new Box(new int [] {9, 14, 3, 7});
+        Box b = new Box(new int [] {15, 19, 1, 4});
+        
+        assertTrue(!a.isBoxOverlapped(b));
+    }
+    
+    @Test
+    public void testIsBoxOverlapped_edge_true() {
+        System.out.println("isBoxOverlapped");
+        Box a = new Box(new int [] {9, 14, 3, 7});
+        Box b = new Box(new int [] {14, 19, 1, 4});
+        
+        assertTrue(a.isBoxOverlapped(b));
+    }
+}
