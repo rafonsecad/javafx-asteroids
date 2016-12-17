817ec532c7e432e780c1bdefd9b4f85e8e3d3fcc
rafael <rafd1788@gmail.com>
2016-12-16 22:34:26 -0500

    change for loop

--- src/main/java/asteriods/elements/Ship.java
+++ src/main/java/asteriods/elements/Ship.java
@@ -46,16 +46,15 @@ public class Ship extends Element {
         List<Point> vectors = getCurrentPosition().changeOrigin(corners);
 
         List<Point> pointsUpdated = new ArrayList<>();
-        for (int i = 0; i < vectors.size(); i++) {
-            double sqrX = Math.pow(vectors.get(i).getX(), 2);
-            double sqrY = Math.pow(vectors.get(i).getY(), 2);
+        for (Point vector : vectors) {
+            double sqrX = Math.pow(vector.getX(), 2);
+            double sqrY = Math.pow(vector.getY(), 2);
 
             double magnitude = Math.sqrt(sqrX + sqrY);
-            double angle = Math.atan2(vectors.get(i).getY(), vectors.get(i).getX());
-            double rotateAngle = angle + radians;
+            double angle = Math.atan2(vector.getY(), vector.getX()) + radians;
 
-            double x = magnitude * Math.cos(rotateAngle) + getCurrentPosition().getX();
-            double y = magnitude * Math.sin(rotateAngle) + getCurrentPosition().getY();
+            double x = magnitude * Math.cos(angle) + getCurrentPosition().getX();
+            double y = magnitude * Math.sin(angle) + getCurrentPosition().getY();
             pointsUpdated.add(new Point(x, y));
         }
 
