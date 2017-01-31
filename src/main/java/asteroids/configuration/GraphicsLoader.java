813682eda120ead9ddcc46a9731a9211312257ea
rafael <rafd1788@gmail.com>
2017-01-30 20:28:28 -0500

    add batik libraries

---
+++ src/main/java/asteroids/configuration/GraphicsLoader.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package asteroids.configuration;
+
+/**
+ *
+ * @author rafael
+ */
+public class GraphicsLoader {
+    
+    private String filename;
+    
+    public GraphicsLoader(){
+        
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+   
+}
